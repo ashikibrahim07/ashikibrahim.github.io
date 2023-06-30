511320ecd98fc65edb149b02953842d42b0e021d
ashikibrahim07 <ashikibrahim7777@gmail.com>
2023-06-30 17:24:08 +0530

    updated

---
+++ assignment 4/script.js
@@ -0,0 +1,21 @@
+var names = new Array();
+names[0]="Yakkov";
+names[1]="Chaikin";
+names[2]="jason";
+names[3]="Jennifer";
+names[4]="John";
+names[5]="Hopkins";
+names[6]="joe";
+names[7]="Chu";
+names[8]="Walter";
+names[9]="Jesse";
+
+for (var i =0; i < names.length; i++) {
+	if (names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+console.log("Goodbye " +names[i])
+ }
+ else{
+ 	console.log("Hello " + names[i])
+ }
+
+ }
